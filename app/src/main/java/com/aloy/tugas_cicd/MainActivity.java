fd27e5236fcb291ba58058ba7f12f36ef291a8a7
Aloysius <aloysiusagias@gmail.com>
2020-10-28 21:10:10 +0700

    init pertama

---
+++ app/src/main/java/com/aloy/tugas_cicd/MainActivity.java
@@ -0,0 +1,14 @@
+package com.aloy.tugas_cicd;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
